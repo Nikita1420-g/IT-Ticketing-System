eb9012e84f109cd7355ea2e0cd66640fd56830bf
Nikita Gupta <nikita@Nikitas-MacBook-Air.local>
2026-02-22 15:33:14 -0800

    made some changes again

--- frontend/src/services/authApi.js
+++ frontend/src/services/authApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL= 'http://localhost:4000/api/auth';
+const API_URL= `${process.env.REACT_APP_API_URL || 'http://localhost:4000/api'}/auth`;
 
 // Register new user
 export const register= async(userData)=>{
